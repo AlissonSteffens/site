623f974c16f63935ab92b475797495202fb6c91b
AlissonSteffens <ali.steffens@gmail.com>
2020-03-07 00:03:39 -0300

    :green_heart: fix build test

--- pages/essays.js
+++ pages/essays.js
@@ -32,30 +32,36 @@ const preparePosts = () =>
 export default () => (
   <Page>
     <Title />
-    
+
     <section className="hero">
-      
       <div className="hero-body">
-        
         <div className="container">
           <div className="columns">
             <div className="column">
               <div className="text-column">
-                <h1 className="title">Alisson's Blog</h1>
-                <p>Aqui eu posto algumas páginas com coisas escritas por mim. A maioria do que eu escrevo é acadêmico, então estes aparecerão como links para os artigos. As postagens (não acadêmicas), são páginas neste site.</p>
+                <h1 className="title">Alisson&apos;s Blog</h1>
+                <p>
+                  Aqui eu posto algumas páginas com coisas escritas por mim. A
+                  maioria do que eu escrevo é acadêmico, então estes aparecerão
+                  como links para os artigos. As postagens (não acadêmicas), são
+                  páginas neste site.
+                </p>
               </div>
             </div>
             <div className="column image-column">
-              <Image
-                src="./static/write.svg"
-                width="350"
-              />
+              <Image src="./static/write.svg" width="350" />
             </div>
           </div>
         </div>
       </div>
-      <svg id="svg" xmlns="http://www.w3.org/2000/svg" viewBox="-300 0 950 270" >
-        <path d="M-314,267 C105,364 400,100 812,279" fill="none" stroke="white" strokeWidth="120" strokeLinecap="round"/>
+      <svg id="svg" xmlns="http://www.w3.org/2000/svg" viewBox="-300 0 950 270">
+        <path
+          d="M-314,267 C105,364 400,100 812,279"
+          fill="none"
+          stroke="white"
+          strokeWidth="120"
+          strokeLinecap="round"
+        />
       </svg>
       <Back />
     </section>
@@ -70,12 +76,17 @@ export default () => (
                   <div className="project" id={p.id}>
                     <figure className="image is-16by9">
                       <Image
-                        src={"./static/essays/"+parseDate(p.date).format('YYYY')+"/"+p.id+"/icon.svg"}
+                        src={
+                          './static/essays/' +
+                          parseDate(p.date).format('YYYY') +
+                          '/' +
+                          p.id +
+                          '/icon.svg'
+                        }
                         width="150"
                       />
                     </figure>
                     <h3>{p.title}</h3>
-                    
                   </div>
                 </a>
               </Link>
@@ -87,92 +98,91 @@ export default () => (
 
     <style jsx>
       {`
-
-      ul {
-        margin: 0;
-        padding: 0 0 10px 0;
-        list-style: none;
-        text-align: left;
-      }
-
-      li {
-        margin: 25px 0;
-        text-decoration: none;
-      }
-
-      li:last-child {
-        margin-bottom: 0;
-      }
-
-      a {
-        text-decoration: none;
-        display: block;
-      }
-
-      b {
-        color: #000;
-        font-size: 17px;
-        display: inline-block;
-        font-weight: normal;
-        line-height: 21px;
-      }
-
-      span {
-        display: block;
-        color: #c1c1c1;
-        font-size: 13px;
-        margin-top: 4px;
-      }
-
-      h1 {
-        margin: 5px 0 40px 0;
-        font-size: 35px;
-        font-weight: 300;
-      }
-
-      @media (min-width: 768px) {
-        a {
-          display: inline-block;
-        }
-
-        span {
-          display: inline-block;
-          margin-left: 20px;
-        }
-
-        b {
-          color: #5a5a5a;
-          font-size: 16px;
-        }
-
         ul {
-          padding-bottom: 0;
-        }
-
-        a:hover b {
-          color: #000;
-        }
-
-        h1 {
-          font-size: 40px;
-          margin-top: 0;
-        }
-      }
-
-      @media (min-width: 992px) {
-        span {
-          opacity: 0;
+          margin: 0;
+          padding: 0 0 10px 0;
+          list-style: none;
+          text-align: left;
         }
 
         li {
-          margin: 22px 0;
+          margin: 25px 0;
+          text-decoration: none;
         }
 
-        a:hover span {
-          opacity: 1;
+        li:last-child {
+          margin-bottom: 0;
         }
-      }
-    `}
+
+        a {
+          text-decoration: none;
+          display: block;
+        }
+
+        b {
+          color: #000;
+          font-size: 17px;
+          display: inline-block;
+          font-weight: normal;
+          line-height: 21px;
+        }
+
+        span {
+          display: block;
+          color: #c1c1c1;
+          font-size: 13px;
+          margin-top: 4px;
+        }
+
+        h1 {
+          margin: 5px 0 40px 0;
+          font-size: 35px;
+          font-weight: 300;
+        }
+
+        @media (min-width: 768px) {
+          a {
+            display: inline-block;
+          }
+
+          span {
+            display: inline-block;
+            margin-left: 20px;
+          }
+
+          b {
+            color: #5a5a5a;
+            font-size: 16px;
+          }
+
+          ul {
+            padding-bottom: 0;
+          }
+
+          a:hover b {
+            color: #000;
+          }
+
+          h1 {
+            font-size: 40px;
+            margin-top: 0;
+          }
+        }
+
+        @media (min-width: 992px) {
+          span {
+            opacity: 0;
+          }
+
+          li {
+            margin: 22px 0;
+          }
+
+          a:hover span {
+            opacity: 1;
+          }
+        }
+      `}
     </style>
   </Page>
 )
--- pages/index.js
+++ pages/index.js
@@ -7,69 +7,143 @@ import moment from 'moment'
 // Layouts
 import Page from '../layouts/page'
 
-
 // Icons
-import { FaTwitter, FaGithub,FaLinkedinIn, FaCamera, FaSchool } from 'react-icons/fa';
-
-
+import {
+  FaTwitter,
+  FaGithub,
+  FaLinkedinIn,
+  FaCamera,
+  FaSchool
+} from 'react-icons/fa'
 
 import projects from '../data/projects'
 
 export default () => (
   <Page>
     <Title />
-    
+
     <section className="hero">
-    
       <div className="hero-body">
         <div className="container">
           <div className="columns">
             <div className="column">
               <div className="text-column">
                 <h1 className="title">Alisson Steffens</h1>
-                <h2 className="subtitle">🐋 Geek, programmer and <a href="https://trakt.tv/users/alissonsteffens" className="link" target="_blank" rel="noopener noreferrer">cinephile</a>. 🌸</h2>
+                <h2 className="subtitle">
+                  🐋 Geek, programmer and{' '}
+                  <a
+                    href="https://trakt.tv/users/alissonsteffens"
+                    className="link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    cinephile
+                  </a>. 🌸
+                </h2>
                 <div className="social">
-                  <a className="social-link" href="https://github.com/AlissonSteffens" target="_blank" rel="noopener noreferrer"><FaGithub/></a>
-                  <a className="social-link" href="https://twitter.com/AlissonSteffens" target="_blank" rel="noopener noreferrer"><FaTwitter/></a>
-                  <a className="social-link" href="https://www.linkedin.com/in/alisson-s-92b58a88/" target="_blank" rel="noopener noreferrer"><FaLinkedinIn/></a>
-                  <a className="social-link" href="https://unsplash.com/@alissonsteffens" target="_blank" rel="noopener noreferrer"><FaCamera/></a>
-                  <a className="social-link" href="http://lattes.cnpq.br/4379708414084009" target="_blank" rel="noopener noreferrer"><FaSchool/></a>
+                  <a
+                    className="social-link"
+                    href="https://github.com/AlissonSteffens"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaGithub />
+                  </a>
+                  <a
+                    className="social-link"
+                    href="https://twitter.com/AlissonSteffens"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaTwitter />
+                  </a>
+                  <a
+                    className="social-link"
+                    href="https://www.linkedin.com/in/alisson-s-92b58a88/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaLinkedinIn />
+                  </a>
+                  <a
+                    className="social-link"
+                    href="https://unsplash.com/@alissonsteffens"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaCamera />
+                  </a>
+                  <a
+                    className="social-link"
+                    href="http://lattes.cnpq.br/4379708414084009"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaSchool />
+                  </a>
                 </div>
-                <p>Graduated in Computer Science (Univali), with emphasis on education, computer systems and games. I like to program 😁, in my free time I usually watch movies, play video games (especially cooperatives) and read comics.</p>
-                <p>I am currently a CAPES PROSUC fellow at Univali's Applied Intelligence Laboratory</p>
-                <p>Also, I'm starting to <Link href="./essays" prefetch><a>blog</a></Link> (in portuguese)</p>
+                <p>
+                  Graduated in Computer Science (Univali), with emphasis on
+                  education, computer systems and games. I like to program 😁,
+                  in my free time I usually watch movies, play video games
+                  (especially cooperatives) and read comics.
+                </p>
+                <p>
+                  I am currently a CAPES PROSUC fellow at Univali&apos;s Applied
+                  Intelligence Laboratory
+                </p>
+                <p>
+                  Also, I&apos;am starting to{' '}
+                  <Link href="./essays" prefetch>
+                    <a>blog</a>
+                  </Link>{' '}
+                  (in portuguese)
+                </p>
               </div>
             </div>
             <div className="column image-column">
-              <Image
-                src="./static/coding.svg"
-                width="350"
-              />
+              <Image src="./static/coding.svg" width="350" />
             </div>
           </div>
         </div>
       </div>
-      <svg id="svg" xmlns="http://www.w3.org/2000/svg" viewBox="-300 0 950 270" >
-        <path d="M-314,267 C105,364 400,100 812,279" fill="none" stroke="white" strokeWidth="120" strokeLinecap="round"/>
+      <svg id="svg" xmlns="http://www.w3.org/2000/svg" viewBox="-300 0 950 270">
+        <path
+          d="M-314,267 C105,364 400,100 812,279"
+          fill="none"
+          stroke="white"
+          strokeWidth="120"
+          strokeLinecap="round"
+        />
       </svg>
     </section>
     <section className="section" id="projects">
       <div className="container">
-        <h1 className="title">Projects <span>more at <a href="https://github.com/AlissonSteffens" target="_blank" rel="noopener noreferrer">github</a></span></h1>
+        <h1 className="title">
+          Projects{' '}
+          <span>
+            more at{' '}
+            <a
+              href="https://github.com/AlissonSteffens"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              github
+            </a>
+          </span>
+        </h1>
         <ul>
           {projects.map(p => (
             <li key={p.id}>
-              
               <a href={p.url} target="_blank" rel="noopener noreferrer">
                 <div className="project" id={p.id}>
                   <figure className="image is-2by1">
                     <Image
-                      src={"./static/projects/"+p.id+".svg"}
+                      src={'./static/projects/' + p.id + '.svg'}
                       width="100"
                     />
                   </figure>
                   <h3>{p.title}</h3>
-                  
                 </div>
               </a>
             </li>
@@ -81,15 +155,21 @@ export default () => (
     <section className="section" id="academic">
       <div className="container">
         <h1 className="title">Academic</h1>
-        <p>You should look at <a className="link" href="http://lattes.cnpq.br/4379708414084009" target="_blank" rel="noopener noreferrer">lattes</a></p>
+        <p>
+          You should look at{' '}
+          <a
+            className="link"
+            href="http://lattes.cnpq.br/4379708414084009"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            lattes
+          </a>
+        </p>
       </div>
     </section>
 
-    <style jsx>{`
-     
-    `}</style>
-    <style global jsx>{`
-
-    `}</style>
+    <style jsx>{``}</style>
+    <style global jsx>{``}</style>
   </Page>
 )
